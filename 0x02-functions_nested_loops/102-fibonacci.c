43a37d659a08936c697d692537492cecbfe1a93b
Temesgen1221 <temezethi@gmail.com>
2023-02-21 11:59:11 -0800

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,24 +7,24 @@
 
 int main(void)
 {
-        int x = 0;
-        long y = 1, z = 2;
+	int x = 0;
+	long y = 1, z = 2;
 
-        while (x < 50)
-        {
-                if (x == 0)
-                printf("%ld", y);
-                else if (x == 1)
-                printf(", %ld", z);
-                else
-                {
-                        z += y;
-                        y = z - y;
-                        printf(", %ld", z);
-                }
-                ++x;
-        }
-        printf("\n");
-        return (0);
+	while (x < 50)
+	{
+		if (x == 0)
+			printf("%ld", y);
+		else if (x == 1)
+			printf(", %ld", z);
+		else
+		{
+			z += y;
+			y = z - y;
+			printf(", %ld", z);
+		}
+		++x;
+	}
+	printf("\n");
+	return (0);
 }
 
