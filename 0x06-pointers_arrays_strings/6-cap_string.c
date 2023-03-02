01e602f8dc16c5675b9d14f0dcc94a9d3039a6a2
Temesgen1221 <temezethi@gmail.com>
2023-03-02 12:22:54 -0800

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,20 +1,32 @@
 #include "main.h"
 
 /**
-* string_toupper - Change all lowercase letters of a string to uppercase
-* @str: THe string to be changed
-* Return: A pointer to the changed string
+* cap_string - Function that capitalizes all words of a string
+* @s: s is the world to be analyzed
+* Return: Always 0.
 */
 
-char *string_toupper(char *str)
+char *cap_string(char *s)
 {
-int index = 0;
+int i = 0;
 
-while (str[index])
+while (s[i] != '\0')
 {
-if (str[index] >= 'a' && str[index] <= 'z')
-str[index] -= 32;
-index++;
+if (s[0] <= 122 && s[0] >= 97)
+{
+s[0] = s[0] - 32;
 }
-return (str);
+if (s[i] == 32 || s[i] == 46 || s[i] == '\t' ||
+s[i] == '\n' || s[i] == 44 || s[i] == 59 ||
+s[i] == '!' || s[i] == '?' || s[i] == '(' ||
+s[i] == ')' || s[i] == '{' || s[i] == '}')
+{
+if (s[i + 1] <= 122 && s[i + 1] >= 97)
+{
+s[i + 1] = s[i + 1] - 32;
+}
+}
+i++;
+}
+return (s);
 }
