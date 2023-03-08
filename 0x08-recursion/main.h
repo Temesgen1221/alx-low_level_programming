5a28274569e012b325c6071141df5b65c87b21d1
Temesgen1221 <temezethi@gmail.com>
2023-03-08 10:52:23 -0800

    main

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,22 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char);
+void _puts_recursion(char*);
+void _print_rev_recursion(char*);
+int _strlen_recursion(char*);
+int factorial(int);
+int _pow_recursion(int, int);
+int _sqrt_recursion(int);
+int _fact(int, int);
+int is_prime_number(int);
+int check_facts(int, int);
+int is_palindrome(char*);
+int _get_rev(char*, char*);
+int compare(char*, char*);
+int wildcmp(char*, char*);
+int full_compare(char*, char*, char*, char *);
+int find_next_c(char**, char*);
+int find_after_sc(char**, char*,char);
+
+#endif
