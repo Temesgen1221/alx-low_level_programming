b0e9307357e2fef3e6022d7a48822158466a1b65
Temesgen1221 <temezethi@gmail.com>
2023-03-14 11:53:39 -0700

    last 5th

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -6,7 +6,7 @@ int _strlen(char *str);
 
 /**
 * argstostr - allocates memory to a concatenation all arguments to a program
-*	with \n between each argument
+* with \n between each argument
 * @ac: number of arguments
 * @av: array of string arguments
 *
@@ -14,38 +14,38 @@ int _strlen(char *str);
 */
 char *argstostr(int ac, char **av)
 {
-int i, j, k = 0, totalStrLen = 0;
-char *ptr;
+        int i, j, k = 0, totalStrLen = 0;
+        char *ptr;
 
-if (ac == 0 || av == NULL)
-{
-return (NULL);
-}
+        if (ac == 0 || av == NULL)
+        {
+                return (NULL);
+        }
 
-for (i = 0; i < ac; i++)
+        for (i = 0; i < ac; i++)
+        { /* calculate length of all arguments +1 for '\n' after each arg */
+                totalStrLen += _strlen(av[i]) + 1;
+        }
 
-totalStrLen += _strlen(av[i]) + 1;
-}
+        ptr = malloc(sizeof(char) * totalStrLen + 1);
+        /* +1 for final null teminator */
+        if (ptr == NULL)
+        {
+                return (NULL);
+        }
 
-ptr = malloc(sizeof(char) * totalStrLen + 1);
+        for (i = 0; i < ac; i++, k++)
+        {
+                for (j = 0; j < _strlen(av[i]); j++, k++)
+                {
+                        ptr[k] = av[i][j];
+                }
+                ptr[k] = '\n'; /* add '\n' after each argument */
+        }
 
-if (ptr == NULL)
-{
-return (NULL);
-}
+        ptr[k] = '\0';
 
-for (i = 0; i < ac; i++, k++)
-{
-for (j = 0; j < _strlen(av[i]); j++, k++)
-{
-ptr[k] = av[i][j];
-}
-ptr[k] = '\n'; /* add '\n' after each argument */
-}
-
-ptr[k] = '\0';
-
-return (ptr);
+        return (ptr);
 }
 
 /**
@@ -56,10 +56,10 @@ return (ptr);
 */
 int _strlen(char *str)
 {
-int i;
+        int i;
 
-for (i = 0; str[i] != '\0'; i++)
-;
+        for (i = 0; str[i] != '\0'; i++)
+                ;
 
-return (i);
+        return (i);
 }
