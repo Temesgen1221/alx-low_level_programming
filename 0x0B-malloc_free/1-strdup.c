26fa7b3b4a2aa2e55b11224f2bae6a0350359a4a
Temesgen1221 <temezethi@gmail.com>
2023-03-20 13:55:32 -0700

    checkup

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,54 +1,54 @@
-#include "main.h"
-#include <stdlib.h>
 #include <stddef.h>
+#include <stdlib.h>
+#include "main.h"
 
 unsigned int _strlen(char *str);
 
 /**
- * _strdup - copies a string to a newly allocated space of memory
+ * _strdup - allocate new memory and copy string
  * @str: the string to be copied
  *
- * Return: a pointer to the newly allocated memory,
- *	or NULL if str = NULL or insufficient memory available
+ * Return: Pointer to the new memory or NULL otherwise
  */
+
 char *_strdup(char *str)
 {
-unsigned int i, len;
-char *ptr;
+	unsigned int i, n;
+	char *ptr;
 
-if (str == NULL)
-{
-return (NULL);
-}
+	if (str == NULL)
+	{
+		return (NULL);
+	}
 
-len = _strlen(str);
+	n = _strlen(str);
 
-ptr = malloc(sizeof(char) * (len + 1));
-if (ptr == NULL)
-{
-return (NULL);
-}
+	ptr = malloc(sizeof(char) * (n + 1));
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
 
-for (i = 0; i <= len; i++)
-{
-ptr[i] = str[i];
-}
+	for (i = 0; i <= n; i++)
+	{
+		ptr[i] = str[i];
+	}
 
-return (ptr);
+	return (ptr);
 }
 
 /**
- * _strlen - calculates the length of string str
+ * _strlen - get the length of string str
  * @str: the string that the length will be calculated
  *
  * Return: the length of the string
  */
+
 unsigned int _strlen(char *str)
 {
-unsigned int i;
+	unsigned int i;
 
-for (i = 0; str[i] != '\0'; i++)
-;
-
-return (i);
+	for (i = 0; str[i] != '\0'; i++)
+		;
+	return (i);
 }
